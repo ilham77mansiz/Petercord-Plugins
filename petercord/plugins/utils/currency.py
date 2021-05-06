4ca7a35490e3e18743e01b95244082ec07379e6c
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-05-06 16:12:44 +0000

    auto: black code format

--- petercord/plugins/utils/currency.py
+++ petercord/plugins/utils/currency.py
@@ -1,18 +1,22 @@
 import json
-from emoji import get_emoji_regexp
 
 import aiohttp
+from emoji import get_emoji_regexp
 
-from petercord import petercord, Message, Config
+from petercord import Config, Message, petercord
 
 CHANNEL = petercord.getCLogger(__name__)
 LOG = petercord.getLogger(__name__)
 
 
-@petercord.on_cmd("cr", about={
-    'header': "use this to convert currency & get exchange rate",
-    'description': "Convert currency & get exchange rates.",
-    'examples': "{tr}cr 1 BTC USD"})
+@petercord.on_cmd(
+    "cr",
+    about={
+        "header": "use this to convert currency & get exchange rate",
+        "description": "Convert currency & get exchange rates.",
+        "examples": "{tr}cr 1 BTC USD",
+    },
+)
 async def cur_conv(message: Message):
     """
     this function can get exchange rate results
@@ -23,10 +27,12 @@ async def cur_conv(message: Message):
             "<a href='https://free.currencyconverterapi.com'>HERE</a> "
             "<code>& add it to Heroku config vars</code> (<code>CURRENCY_API</code>)",
             disable_web_page_preview=True,
-            parse_mode="html", del_in=0)
+            parse_mode="html",
+            del_in=0,
+        )
         return
 
-    filterinput = get_emoji_regexp().sub(u'', message.input_str)
+    filterinput = get_emoji_regexp().sub("", message.input_str)
     curcon = filterinput.upper().split()
 
     if len(curcon) == 3:
@@ -37,12 +43,14 @@ async def cur_conv(message: Message):
 
     if amount.isdigit():
         async with aiohttp.ClientSession() as ses:
-            async with ses.get("https://free.currconv.com/api/v7/convert?"
-                               f"apiKey={Config.CURRENCY_API}&q="
-                               f"{currency_from}_{currency_to}&compact=ultra") as res:
+            async with ses.get(
+                "https://free.currconv.com/api/v7/convert?"
+                f"apiKey={Config.CURRENCY_API}&q="
+                f"{currency_from}_{currency_to}&compact=ultra"
+            ) as res:
                 data = json.loads(await res.text())
         try:
-            result = data[f'{currency_from}_{currency_to}']
+            result = data[f"{currency_from}_{currency_to}"]
         except KeyError:
             LOG.info(data)
             await message.err("invalid response from api !")
@@ -51,10 +59,12 @@ async def cur_conv(message: Message):
         result = round(result, 5)
         await message.edit(
             "**CURRENCY EXCHANGE RATE RESULT:**\n\n"
-            f"`{amount}` **{currency_to}** = `{result}` **{currency_from}**")
+            f"`{amount}` **{currency_to}** = `{result}` **{currency_from}**"
+        )
         await CHANNEL.log("`cr` command executed sucessfully")
 
     else:
         await message.edit(
             r"`This seems to be some alien currency, which I can't convert right now.. (⊙_⊙;)`",
-            del_in=0)
+            del_in=0,
+        )
