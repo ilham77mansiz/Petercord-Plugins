4f15018798e9f94699849081e02e06568bab0979
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-06 14:15:41 +0700

    Create __init__.py

---
+++ petercord/__init__.py
@@ -0,0 +1,6 @@
+from petercord.logger import logging  # noqa
+from petercord.config import Config, get_version  # noqa
+from petercord.core import (  # noqa
+    Petercord-Plugins, filters, Message, get_collection, pool)
+
+petercord = Petercord-Plugins()  
