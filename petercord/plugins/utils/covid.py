42065eb88878f3a8a4d89ae5d5daf6be0e5a5a39
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-05-06 16:10:58 +0000

    auto: black code format

--- petercord/plugins/utils/covid.py
+++ petercord/plugins/utils/covid.py
@@ -1,23 +1,26 @@
-
 from covid import Covid
 
-from petercord import petercord, Message, pool
+from petercord import Message, petercord, pool
 
 
-@petercord.on_cmd("covid", about={
-    'header': "see covid details",
-    'description': "The current real time situation of the COVID-19 patients reported in worldwide",
-    'flags': {'-l': "list countries"},
-    'petercord': "{tr}covid [flag | country]",
-    'examples': ["{tr}covid -l", "{tr}covid", "{tr}covid india"]})
+@petercord.on_cmd(
+    "covid",
+    about={
+        "header": "see covid details",
+        "description": "The current real time situation of the COVID-19 patients reported in worldwide",
+        "flags": {"-l": "list countries"},
+        "petercord": "{tr}covid [flag | country]",
+        "examples": ["{tr}covid -l", "{tr}covid", "{tr}covid india"],
+    },
+)
 async def covid(message: Message):
     await message.edit("`fetching covid ...`")
     covid_ = await pool.run_in_thread(Covid)("worldometers")
     country = message.input_str
     result = ""
-    if '-l' in message.flags:
+    if "-l" in message.flags:
         result += "<u>Covid Supported Countries</u>\n\n`"
-        result += '` , `'.join(sorted(filter(lambda x: x, covid_.list_countries())))
+        result += "` , `".join(sorted(filter(lambda x: x, covid_.list_countries())))
         result += "`"
     elif country:
         try:
@@ -36,12 +39,16 @@ async def covid(message: Message):
         result += f"**total tests** : `{data['total_tests']}`\n"
         result += f"**total tests per million** : `{data['total_tests_per_million']}`\n"
         result += f"**total cases per million** : `{data['total_cases_per_million']}`\n"
-        result += f"**total deaths per million** : `{data['total_deaths_per_million']}`\n"
+        result += (
+            f"**total deaths per million** : `{data['total_deaths_per_million']}`\n"
+        )
         result += f"**population** : `{data['population']}`\n"
     else:
         result += "<u>Covid Status in the world</u>\n\n"
         result += f"**total active cases** : `{covid_.get_total_active_cases()}`\n"
-        result += f"**total confirmed cases** : `{covid_.get_total_confirmed_cases()}`\n"
+        result += (
+            f"**total confirmed cases** : `{covid_.get_total_confirmed_cases()}`\n"
+        )
         result += f"**total deaths** : `{covid_.get_total_deaths()}`\n"
         result += f"**total recovered** : `{covid_.get_total_recovered()}`\n"
     await message.edit_or_send_as_file(result)
